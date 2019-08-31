a3706b290883077dfd4a0045749509c1f41d38e7
Nick Martinez <nick@xdmtk.org>
2019-08-30 19:16:51 -0700

    Setup initial starzone

--- hacks/starscape.c
+++ hacks/starscape.c
@@ -10,6 +10,7 @@
 */
 
 #include <math.h>
+#include <assert.h>
 #include "screenhack.h"
 #include "alpha.h"
 #include "../clion_include/X.h"
@@ -17,6 +18,9 @@
 
 #define countof(x) (sizeof(x)/sizeof(*(x)))
 #define ABS(x) ((x)<0?-(x):(x))
+struct origin_field{
+    XPoint ul, ur, bl, br;
+};
 
 struct star {
     XPoint location;
@@ -26,17 +30,14 @@ struct star {
 struct state {
     Display *dpy;
     Window window;
-
+    struct origin_field origin;
     XColor *colors, last_color;
     GC gc;
     XWindowAttributes xgwa;
     Colormap colormap;
     XGCValues gcv;
-
-    int window_w, window_h;
-    struct origin_field{
-        XPoint ul, ur, bl, br;
-    } origin;
+    struct star stars[];
+    int window_w, window_h, delay, star_count;
 
 
 };
@@ -45,15 +46,14 @@ static void init_origin(struct state * st) {
     /* Setup a square field in the center of the screen as origin for star generation */
     st->origin.ul.y = st->origin.ur.y = (st->window_h/2)+(int)(st->window_h*.25);
     st->origin.bl.y = st->origin.br.y = (st->window_h/2)-(int)(st->window_h*.25);
-    st->origin.ul.x = st->origin->bl.x = (st->window_w/2)-(int)(st->window_w*.25);
-    st->origin.ur.x = st->origin->br.x = (st->window_w/2)+(int)(st->window_w*.25);
+    st->origin.ul.x = st->origin.bl.x = (st->window_w/2)-(int)(st->window_w*.25);
+    st->origin.ur.x = st->origin.br.x = (st->window_w/2)+(int)(st->window_w*.25);
 }
 
 static void * starscape_init (Display *dpy, Window window) {
 
     /* Setup state variable */
     struct state *st = (struct state *) calloc (1, sizeof(*st));
-    init_origin(st);
     /* Default display given to us by Xscreensaver */
     st->dpy = dpy;
 
@@ -66,10 +66,12 @@ static void * starscape_init (Display *dpy, Window window) {
      * we are going to draw on, and set it in xgwa ( x get window attributes) ) */
     XGetWindowAttributes (st->dpy, st->window, &st->xgwa);
 
-    /* Get the dimensions of the window */
-    st->width = st->xgwa.width;
-    st->height = st->xgwa.height;
 
+    /* Get the dimensions of the window */
+    st->window_w = st->xgwa.width;
+    st->window_h = st->xgwa.height;
+
+    init_origin(st);
     /* Setting up the colormap. not exactly sure what this does yet */
     st->colormap = st->xgwa.colormap;
     /* Variable to hold properties that we can attribute the the Graphics Context ( GC ) */
@@ -80,23 +82,31 @@ static void * starscape_init (Display *dpy, Window window) {
     return st;
 }
 
+static void generate_initial_stars(struct state * st) {
+    int i, initial_stars = random() % 25;
+    st->stars = malloc(sizeof(struct star)*initial_stars);
+    assert(st->stars);
+
+
+
+}
 
 static unsigned long starscape_draw (Display *dpy, Window window, void *closure) {
     struct state *st = (struct state *) closure;
-    XDrawLines(st->dpy, st->window, st->gc,
-            {st->origin.bl, st->origin.br, st->origin.ur, st->origin.ul}, 4, CoordModeOrigin);
+
+
 
     return st->delay;
 }
 
 static void starscape_reshape (Display *dpy, Window window, void *closure,
         unsigned int w, unsigned int h) {
+
     struct state *st = (struct state * ) closure;
     XGetWindowAttributes (st->dpy, st->window, &st->xgwa);
 
-    /* Get the dimensions of the window */
-    st->window_w = st->xgwa.width;
-    st->window_h  = st->xgwa.height;
+    st->window_w = st->xgwa.width; st->window_h  = st->xgwa.height;
+    init_origin(st);
     return;
 }
 
